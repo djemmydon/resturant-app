fbb8a0341591bc581ab88c06bf507be32f2ef408
unknown <okinopatrick0@gmail.com>
2023-01-21 03:19:29 +0100

    Updating the frontend

--- sanity/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'dgcyry4z',
-    dataset: 'production'
-  }
-})
--- sanity/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import post from './post'
-import author from './author'
-import product from './product'
-
-export const schemaTypes = [product, author, category, blockContent]
